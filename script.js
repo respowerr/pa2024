6bfc2a5655a4497790149f6c5513616f1c073a51
SnikiEP <enzopizzuti17@hotmail.fr>
2024-03-26 00:06:15 +0100

    new architecture website

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('menu-btn').addEventListener('click', function() {
+        document.getElementById('menu').classList.toggle('active');
+    });
+});
