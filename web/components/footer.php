3787eda2c30a3927cab4b7d270d8ce57c0fb0ed3
Amlezia <alia.mezouar2@gmail.com>
2024-05-01 16:46:54 +0200

    Fix some translations

--- web/components/footer.php
+++ web/components/footer.php
@@ -1,8 +1,8 @@
 <?php
-    include $_SERVER['DOCUMENT_ROOT'] . '/Multilingue/translations.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/multilingue/translations.php';
 ?>
 
 <footer class="custom-footer">
-    © 2024 Own by callidos group <?php echo $translations['Footer']; ?>
+    <?php echo $translations['Footer']; ?>
 </footer>
 <link rel="stylesheet" href="/styles/footer.css">
