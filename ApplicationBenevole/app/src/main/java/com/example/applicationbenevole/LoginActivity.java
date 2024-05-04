a400b4ad4731ed5c43b8d610d9da74d1afbb3e3c
Amlezia <alia.mezouar2@gmail.com>
2024-05-05 00:34:04 +0200

    Finish Login (il faut fix le systeme de deconnexion qui marche pas trop)

--- ApplicationBenevole/app/src/main/java/com/example/applicationbenevole/LoginActivity.java
+++ ApplicationBenevole/app/src/main/java/com/example/applicationbenevole/LoginActivity.java
@@ -1,33 +1,30 @@
 package com.example.applicationbenevole;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private final String logTag = "APP_BENEVOLE";
     private EditText usernameEditText;
     private EditText passwordEditText;
-    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +33,9 @@ public class LoginActivity extends AppCompatActivity {
 
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
-        loginButton = findViewById(R.id.loginButton);
+        Button loginButton = findViewById(R.id.loginButton);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loginUser();
-            }
-        });
+        loginButton.setOnClickListener(v -> loginUser());
     }
 
     private void loginUser() {
@@ -60,46 +52,69 @@ public class LoginActivity extends AppCompatActivity {
             return;
         }
 
-        String url = "http://192.168.63.136:8080/account/login"; //A mettre en var env
+        String url = getResources().getString(R.string.server_url_account);
 
         JSONObject jsonBody = new JSONObject();
         try {
             jsonBody.put("username", username);
             jsonBody.put("password", password);
         } catch (JSONException e) {
-            e.printStackTrace();
+            Log.e(logTag, "Erreur de création du JSONBody:" + Arrays.toString(e.getStackTrace()));
             Toast.makeText(LoginActivity.this, "Erreur de création du JSONBody", Toast.LENGTH_SHORT).show();
             return;
         }
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            String status = response.getString("status");
-                            if (status.equals("success")) {
-                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                                finish();
-                            } else {
-                                Toast.makeText(LoginActivity.this, "Erreur de connexion, veuillez réessayer1.", Toast.LENGTH_SHORT).show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            Toast.makeText(LoginActivity.this, "Erreur de connexion, veuillez réessayer2.", Toast.LENGTH_SHORT).show();
+                response -> {
+                    try {
+                        Log.d(logTag, "response du serveur: " + response);
+                        String accessToken = response.getString("accessToken");
+                        if (!TextUtils.isEmpty(accessToken)) {
+                            Log.i(logTag, "Connexion réussie");
+                            saveUserSession();
+                            openMainActivity(accessToken);
+                            Toast.makeText(LoginActivity.this, "Connexion réussie", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Erreur de login. Veuillez réessayer", Toast.LENGTH_SHORT).show();
                         }
+                    } catch (JSONException e) {
+                        Log.e(logTag, Arrays.toString(e.getStackTrace()));
+                        Toast.makeText(LoginActivity.this, "Erreur Server", Toast.LENGTH_SHORT).show();
                     }
                 },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                        Toast.makeText(LoginActivity.this, "Erreur de connexion, veuillez réessayer3.", Toast.LENGTH_SHORT).show();
+                error -> {
+                    String errorMessage;
+                    if (error.networkResponse != null && error.networkResponse.data != null) {
+                        errorMessage = new String(error.networkResponse.data, StandardCharsets.UTF_8);
+                        Log.e(logTag, errorMessage);
+                    } else {
+                        errorMessage = "Error: " + error.getMessage();
+                        Log.e(logTag, errorMessage);
                     }
-                });
+
+                    Toast.makeText(LoginActivity.this, "Une erreur s'est produite lors de la connexion. Veuillez vérifier votre connexion Internet et réessayer.", Toast.LENGTH_SHORT).show();
+                }
+
+        );
 
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(jsonObjectRequest);
     }
+
+    private void saveUserSession() {
+        SharedPreferences sharedPref = getSharedPreferences("user_session", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean("is_logged_in", true);
+        editor.apply();
+    }
+
+    private void openMainActivity(String accessToken) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.putExtra("access_token", accessToken);
+        Log.w(logTag, String.valueOf(intent));
+        startActivity(intent);
+    }
+
 
 }
--- ApplicationBenevole/app/src/main/java/com/example/applicationbenevole/MainActivity.java
+++ ApplicationBenevole/app/src/main/java/com/example/applicationbenevole/MainActivity.java
@@ -1,54 +1,110 @@
 package com.example.applicationbenevole;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String logTag = "APP_BENEVOLE";
+    private SharedPreferences sharedPreferences;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPreferences = getSharedPreferences("user_session", MODE_PRIVATE);
 
         TextView welcomeTextView = findViewById(R.id.welcomeTextView);
-        Button loginButton = findViewById(R.id.loginButton);
+        loginButton = findViewById(R.id.loginButton);
         Button planningButton = findViewById(R.id.planningButton);
         Button profileButton = findViewById(R.id.profileButton);
         Button activitiesButton = findViewById(R.id.activitiesButton);
 
+        boolean isLoggedIn = sharedPreferences.getBoolean("is_logged_in", false);
+        updateLoginButton(isLoggedIn);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                if (isLoggedIn) {
+                    logoutUser();
+                } else {
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }
+
             }
+
         });
+
+        String accessToken = getIntent().getStringExtra("access_token");
+        if (accessToken != null) {
+            Log.d(logTag, accessToken);
+        } else {
+            Log.d(logTag, "Access token non trouvé dans l'intent");
+        }
 
         planningButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, PlanningActivity.class));
+                String accessToken = getIntent().getStringExtra("access_token");
+                if (accessToken != null) {
+                    Intent intent = new Intent(MainActivity.this, PlanningActivity.class);
+                    intent.putExtra("access_token", accessToken);
+                    Log.d(logTag, accessToken);
+                    startActivity(intent);
+                } else {
+
+                    Toast.makeText(MainActivity.this, "Vous devez vous connecter pour accéder au planning.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
 
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                if (isLoggedIn) {
+                    startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                } else {
+                    Toast.makeText(MainActivity.this, "Vous devez vous connecter pour accéder au profil.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         activitiesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ActivitiesActivity.class));
+                if (isLoggedIn) {
+                    startActivity(new Intent(MainActivity.this, ActivitiesActivity.class));
+                } else {
+                    Toast.makeText(MainActivity.this, "Vous devez vous connecter pour accéder aux activités.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
+
+    private void updateLoginButton(boolean isLoggedIn) {
+        if (isLoggedIn) {
+            loginButton.setText("Se déconnecter");
+        } else {
+            loginButton.setText("Se connecter");
+        }
+    }
+
+    private void logoutUser() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove("is_logged_in");
+        editor.apply();
+        updateLoginButton(false);
+    }
 }
