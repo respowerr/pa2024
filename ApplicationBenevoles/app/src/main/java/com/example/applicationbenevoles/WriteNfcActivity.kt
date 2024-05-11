dffe427917b515b99d81fa6550bf3394230a355b
Amlezia <alia.mezouar2@gmail.com>
2024-05-11 19:45:16 +0200

    Update nfc link to api user

--- ApplicationBenevoles/app/src/main/java/com/example/applicationbenevoles/WriteNfcActivity.kt
+++ ApplicationBenevoles/app/src/main/java/com/example/applicationbenevoles/WriteNfcActivity.kt
@@ -16,14 +16,21 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONException
 
 class WriteNfcActivity : AppCompatActivity() {
 
-    private val logTag = "NFC_APP"
+    private val logTag = "APP_BENEVOLE "
     private lateinit var adapter: NfcAdapter
     private lateinit var editText: EditText
     private lateinit var writeNfcButton: Button
     private var tagFromIntent: Tag? = null
+
+    private lateinit var accessToken: String
+
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,13 +40,15 @@ class WriteNfcActivity : AppCompatActivity() {
         editText = findViewById(R.id.editText)
         writeNfcButton = findViewById(R.id.writeNfcButton)
 
+
+        accessToken = intent.getStringExtra("access_token") ?: ""
+        Log.d(logTag, accessToken)
+
+
         writeNfcButton.setOnClickListener {
-            tagFromIntent?.let { tag ->
-                val nfcText = editText.text.toString()
-                writeNfc(tag, nfcText)
-            } ?: run {
-                Toast.makeText(this, "Please tap an NFC tag", Toast.LENGTH_SHORT).show()
-            }
+            val nfcText = editText.text.toString()
+            Log.d(logTag, "Write NFC button clicked with text: $nfcText")
+            checkIdExistence(nfcText, accessToken)
         }
 
         initNfcAdapter()
@@ -48,6 +57,9 @@ class WriteNfcActivity : AppCompatActivity() {
     private fun initNfcAdapter() {
         val nfcManager = getSystemService(Context.NFC_SERVICE) as NfcManager
         adapter = nfcManager.defaultAdapter
+        if (adapter == null) {
+            Log.e(logTag, "NFC Adapter is null. This device may not support NFC.")
+        }
     }
 
     private fun enableNfcForegroundDispatch() {
@@ -57,6 +69,7 @@ class WriteNfcActivity : AppCompatActivity() {
                 this, 0, intent, PendingIntent.FLAG_MUTABLE
             )
             adapter.enableForegroundDispatch(this, nfcPendingIntent, null, null)
+            Log.d(logTag, "NFC foreground dispatch enabled")
         } catch (ex: IllegalStateException) {
             Log.e(logTag, "Error enabling NFC foreground dispatch", ex)
         }
@@ -65,6 +78,7 @@ class WriteNfcActivity : AppCompatActivity() {
     private fun disableNfcForegroundDispatch() {
         try {
             adapter.disableForegroundDispatch(this)
+            Log.d(logTag, "NFC foreground dispatch disabled")
         } catch (ex: IllegalStateException) {
             Log.e(logTag, "Error disabling NFC foreground dispatch", ex)
         }
@@ -103,16 +117,64 @@ class WriteNfcActivity : AppCompatActivity() {
         if (tagFromIntent != null) {
             Log.d(logTag, "NFC_TAG received")
             val nfcText = editText.text.toString()
-            writeNfc(tagFromIntent!!, nfcText)
+            checkIdExistence(nfcText, accessToken)
         } else {
             Log.d(logTag, "No NFC_TAG received")
             Toast.makeText(this, "Please tap an NFC tag", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun writeNfc(tag: Tag, nfcText: String) {
-        Log.d(logTag, "Writing NFC tag...")
-        val ndef = Ndef.get(tag)
+    private fun checkIdExistence(nfcText: String, accessToken: String) {
+        val url = getString(R.string.server_url_info) + "/all"
+        Log.d(logTag, "Checking ID existence for text: $nfcText")
+        val stringRequest = object : StringRequest(Method.GET, url,
+            { response ->
+                try {
+                    Log.d(logTag, "Response received: $response")
+                    val jsonArray = JSONArray(response)
+
+                    var isValid = false
+                    for (i in 0 until jsonArray.length()) {
+                        val user = jsonArray.getJSONObject(i)
+                        val userId = user.getInt("id").toString()
+                        Log.w(logTag, userId)
+                        if (userId == nfcText) {
+                            isValid = true
+                            break
+                        }
+                    }
+
+                    if (isValid) {
+                        writeNfcTag(nfcText)
+                    } else {
+                        Toast.makeText(this, "ID is not valid", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: JSONException) {
+                    Log.e(logTag, "Error parsing validation JSON: ${e.message}")
+                }
+            },
+            { error ->
+                val errorMessage = "Error: " + error.message
+                Log.e(logTag, "Error validating ID: $errorMessage")
+                error.printStackTrace()
+                Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            override fun getHeaders(): Map<String, String> {
+                val headers: MutableMap<String, String> = HashMap()
+                headers["Authorization"] = "Bearer $accessToken"
+                Log.d(logTag, headers.toString())
+                return headers
+            }
+        }
+
+        Volley.newRequestQueue(this).add(stringRequest)
+    }
+
+
+
+    private fun writeNfcTag(nfcText: String) {
+        val ndef = Ndef.get(tagFromIntent)
         if (ndef != null) {
             try {
                 ndef.connect()
@@ -140,4 +202,3 @@ class WriteNfcActivity : AppCompatActivity() {
         return NdefMessage(arrayOf(ndefRecord))
     }
 }
-
